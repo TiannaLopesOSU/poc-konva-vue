b97cdb0026ec7812b752ec4527509c14977410ea
TiannaLopesOSU <lopesti@oregonstate.edu>
2023-10-17 21:21:13 -0700

    Adding konva to proj

--- src/components/KonvaDrawing.vue
+++ src/components/KonvaDrawing.vue
@@ -1,43 +1,28 @@
 <template>
-  <div>
-    <v-stage :config="stageConfig">
-      <v-layer>
-        <v-rect
-          :config="rectConfig"
-          @click="changeColor"
-          :stroke="rectStroke"
-          :strokeWidth="3"
-        />
-      </v-layer>
-    </v-stage>
-    <button @click="changeColor">Change Color</button>
-  </div>
+  <v-stage :config="configKonva">
+    <v-layer>
+      <v-circle :config="configCircle"></v-circle>
+    </v-layer>
+  </v-stage>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      stageConfig: {
-        width: 400,
-        height: 400,
+      configKonva: {
+        width: 200,
+        height: 200,
       },
-      rectConfig: {
-        x: 50,
-        y: 50,
-        width: 100,
-        height: 100,
-        fill: "green",
+      configCircle: {
+        x: 100,
+        y: 100,
+        radius: 70,
+        fill: "red",
+        stroke: "black",
+        strokeWidth: 4,
       },
-      rectStroke: "black", // Border color
     };
-  },
-  methods: {
-    changeColor() {
-      // Toggle between green and blue
-      this.rectConfig.fill =
-        this.rectConfig.fill === "green" ? "blue" : "green";
-    },
   },
 };
 </script>
